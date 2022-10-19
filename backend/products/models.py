d498d674e2c9d3e0b41c00d1f2878017c9a011d3
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-10-19 04:54:51 +0200

    saledPrice, getDiscount Functions

--- backend/products/models.py
+++ backend/products/models.py
@@ -7,3 +7,12 @@ class products(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=15, decimal_places=2, default=99.99)  # type: ignore
+
+    # if just wanna print saled_price without using serializers !!
+
+    @property
+    def saled_price(self):
+        return "%.2f" % (float(self.price) * 0.8)
+
+    def get_discount(self):
+        return "123"
