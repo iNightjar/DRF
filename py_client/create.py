852a6539fd4dd343f8c48784d51eba88f7515085
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-10-19 14:23:08 +0200

    request get,post tests with generics CBV

---
+++ py_client/create.py
@@ -0,0 +1,13 @@
+import requests
+
+
+# do request though python clint to the django project
+endpoint = "http://localhost:8000/api/products/"
+
+data = {
+    "title": "smart screen",
+    "price": "101002",
+}
+
+get_response = requests.post(endpoint, json=data)  # API Application programming interface
+print(get_response.json())
