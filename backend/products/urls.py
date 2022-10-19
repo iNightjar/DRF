31cd7ceb83f7d4f8680f2de9129fd0211a4b29b7
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-10-19 14:24:10 +0200

    url route added (listall/)

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.product_create_view),
+
     path('<int:pk>/', views.productDetailAPIView.as_view()),
 
+    path('listall/', views.productsList.as_view()),
 ]
