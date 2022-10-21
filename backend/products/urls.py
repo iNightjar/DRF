6831b523fcf23d52e240185c25efc3cb674a413d
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-10-21 10:11:01 +0200

    route url for productdestory, productupdate

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     # FBV -> create_list_FBV
     path('', views.product_list_create_view),
     path('<int:pk>/', views.product_detail_view),
+    path('<int:pk>/update/', views.product_update_view),
+    path('<int:pk>/delete/', views.product_delete_view),
 ]
