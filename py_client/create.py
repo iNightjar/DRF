027cef3ec21f19ab0661db9878ce2e05f8eb22f5
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-10-22 02:49:57 +0200

    testing post request

--- py_client/create.py
+++ py_client/create.py
@@ -3,6 +3,10 @@ import requests
 
 # do request though python clint to the django project
 endpoint = "http://localhost:8000/api/products/"
+# http://localhost:8000/admin/
+# session -> post data
+# selenuim
+
 
 data = {
     "title": "visual studio code",
