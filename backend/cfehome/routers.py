f46dd0029d72c357fe3c209ced1087d341327c59
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-10-23 17:49:24 +0200

    remove the print(router.urls) statement

--- backend/cfehome/routers.py
+++ backend/cfehome/routers.py
@@ -3,5 +3,4 @@ from products.viewsets import ProductGenericViewSet
 
 router = DefaultRouter()
 router.register('products', ProductGenericViewSet, basename='products')
-print(router.urls)
 urlpatterns = router.urls
