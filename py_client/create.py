764ae46ff67d6c4df84ef1b0532e4a7e133421ae
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-10-23 07:17:34 +0200

    check user authentication token before create product

--- py_client/create.py
+++ py_client/create.py
@@ -1,6 +1,8 @@
 import requests
 
 
+headers = {'Authorization': 'Bearer 20018e27f7707f46a8e8418f993a643e9ecdc8c2'}
+
 # do request though python clint to the django project
 endpoint = "http://localhost:8000/api/products/"
 # http://localhost:8000/admin/
@@ -11,8 +13,8 @@ endpoint = "http://localhost:8000/api/products/"
 data = {
     "title": "visual studio code",
 
-    "price": "101002",
+    "price": 200.2
 }
 
-get_response = requests.post(endpoint, json=data)  # API Application programming interface
+get_response = requests.post(endpoint, json=data, headers= headers) # API Application programming interface
 print(get_response.json())
