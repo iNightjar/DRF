e6884a2405cffeb285f4cd974e99b3506ed8bf6b
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-10-23 08:13:43 +0200

    Auth, Perm Default Settings

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -127,3 +128,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# REST_FRAMEWORK Settings
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "api.authentication.TokenAuthentication"
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        # Only Get to use for everyone
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ]
+}
