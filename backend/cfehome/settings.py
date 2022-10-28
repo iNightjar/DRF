cb7277aa38c4e0709a248b422c7e47f215f1e4fe
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-10-28 13:34:18 +0200

    add pagination class

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -138,5 +138,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         # Only Get to use for everyone
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
 }
