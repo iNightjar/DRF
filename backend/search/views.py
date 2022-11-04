852aae474e36b1f9440ffe09dfc1a46e1b627b3e
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-11-04 23:33:42 +0200

    get_queryset custom method

--- backend/search/views.py
+++ backend/search/views.py
@@ -7,12 +7,12 @@ class SearchListView(generics.ListAPIView):
     serializer_class = productSerializer
     
     def get_queryset(self,*args, **kwargs):
-        querySet= super().get_queryset(*args, **kwargs)
+        qs= super().get_queryset(*args, **kwargs)
         q = self.request.GET.get('q')
         results = products.objects.none()
         if q is not None:
             user = None
             if self.request.user.is_authenticated:
                 user = self.request.user
-            results = querySet.search(q, user = user)
-        return results 
+            results = qs.search(q, user=user)
+        return results
