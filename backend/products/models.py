2b23470a2e9f2d969f927a46fcc24ea73c94690a
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-11-04 23:34:40 +0200

    ProductQuerySet, ProductManager Classes

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,10 +1,31 @@
 from django.conf import settings
 from django.db import models
-
+from django.db.models import Q
 # Create your models here.
 
 User = settings.AUTH_USER_MODEL # auth.User
 
+
+class ProductQuerySet(models.QuerySet):
+    def is_public(self):
+        return self.filter(public=True)
+
+
+    def search(self,query, user=None):
+        lookup = Q(title__icontains=query) | Q(content__icontains=query)
+        qs = self.is_public().filter(lookup)
+        if user is not None:
+            qs2 = self.filter(user=user).filter(lookup)
+            qs = (qs | qs2).distinct()
+        return qs
+
+class ProductManager(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        return ProductQuerySet(self.model, using=self._db)
+
+
+    def search(self, query, user=None):
+        return self.get_queryset().search(query, user=user)
 
 class products(models.Model):
     # pk -> integer
@@ -16,6 +37,7 @@ class products(models.Model):
     public = models.BooleanField(default=True)
 
     # if just wanna print saled_price without using serializers !!
+    objects= ProductManager()
 
     @property
     def saled_price(self):
