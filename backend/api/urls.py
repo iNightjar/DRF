8abb5dcd1f9898d3b0c513f538caa0f989562e44
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-11-13 00:04:48 +0200

    adding urls for jwt tokens

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,12 +2,21 @@ from django.urls import path
 from . import views
 # from .views import api_home
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 # localhost:8000/api/
 urlpatterns = [
 
     path('', views.api_home),  # localhost:8000/api/
     path('auth/', obtain_auth_token),
-    
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
 ]
