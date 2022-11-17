340805c69d41bc2715e768dc6a01e597f47c18e1
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-11-17 04:25:39 +0200

    handleLogin, writeToContainer methods

--- js_client/client.js
+++ js_client/client.js
@@ -1,4 +1,5 @@
 const loginForm = document.getElementById("login-form");
+const contentContainer = document.getElementById("content-container");
 const baseEndpoint = "http://localhost:8000/api";
 if (loginForm) {
     // handle this login form
@@ -24,21 +25,40 @@ function handleLogin(event) {
             console.log(response);
             return response.json();
         })
-        .then(handleAuthData)
+        .then((authData) => {
+            handleAuthData(authData, getProductList);
+        })
         .catch((err) => {
             console.log("err", err);
         });
 }
 
-function handleAuthData(authData) {
+function handleAuthData(authData, callback) {
     localStorage.setItem("access", authData.access);
     localStorage.setItem("refresh", authData.refresh);
+    if (callback) {
+        callback();
+    }
 }
 
-// function getProductList() {
-//     const endpoint = `${baseEndpoint}/products/`
-//     const options = {
-//         method: "GET",
-//         headers
-//     }
-// }
+function writeToContainer(data) {
+    if (contentContainer) {
+        contentContainer.innerHTML = "<pre>" + JSON.stringify(data) + "</pre>";
+    }
+}
+
+function getProductList() {
+    const endpoint = `${baseEndpoint}/products/`;
+    const options = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }
+        .fetch(endpoint, options)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            writeToContainer(data);
+        });
+}
